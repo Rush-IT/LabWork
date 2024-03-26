bc7347981fbf298129dab6698249795dc97a10ca
Rush-IT <78534165+Rush-IT@users.noreply.github.com>
2024-03-27 04:15:10 +0500

    Change code

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -2,85 +2,128 @@
 #include "mpi.h"
 #include <stdio.h>
 
-#define N 256
 
-void print_results(char* prompt, int result[N][N]);
+#define ROW 1000
+#define COL 1000
+
+double mA[ROW][COL];
+double mB[ROW][COL];
+double mR[ROW][COL];
+
+double startTime;
+double endTime;
+
+void MakeMatrix(double matrix[ROW][COL], int n)
+{
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            matrix[i][j] = i + j * n;
+        }
+    }
+}
+void printArray()
+{
+    int i, j;
+    for (i = 0; i < ROW; i++) {
+        printf("\n");
+        for (j = 0; j < COL; j++)
+            printf("%8.2f  ", mA[i][j]);
+    }
+    printf("\n---------------\n");
+    for (i = 0; i < ROW; i++) {
+        printf("\n");
+        for (j = 0; j < COL; j++)
+            printf("%8.2f  ", mB[i][j]);
+    }
+    printf("\n---------------\n");
+    for (i = 0; i < ROW; i++) {
+        printf("\n");
+        for (j = 0; j < COL; j++)
+            printf("%8.2f  ", mR[i][j]);
+    }
+    printf("\n\n");
+}
 
 int main(int argc, char* argv[])
 {
-    int rank; // Ранг процесса
-    int size; // Номер процесса
-    int i, j, k, rows = 0;
-    int buff = 0;
-
-    int mA[N][N];
-    int mB[N][N];
-    int mResult[N][N];
-
-    double startTime, endTime;
+    int rank, size;
+    int numCalculate;// Кол-во вычислений на один процесс
+    int lowBound, upBound;
 
     MPI_Status status;
     MPI_Request request;
 
-    MPI_Init(&argc, &argv); //initialize MPI operations
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank); //get the rank
-    MPI_Comm_size(MPI_COMM_WORLD, &size); //get number of processes
-    MPI_Barrier(MPI_COMM_WORLD);
+    
 
-    if (rank == 0) 
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0)
     {
-        // Инициализация матриц А и B
-        for (i = 0; i < N; i++)
-            for (j = 0; j < N; j++)
-                mA[i][j] = rand() % 10;
-        
-        for (i = 0; i < N; i++)
-            for (j = 0; j < N; j++)
-                mB[i][j] = rand() % 10;
+        MakeMatrix(mA, 1);
+        MakeMatrix(mB, 2);
 
         startTime = MPI_Wtime();
-        for (i = 0; i < size; i++)
+
+        numCalculate = ROW / (size - 1);
+
+        for (int i = 0; i < size; i++)
         {
-            if (i < (N % (size - 1)))
-                rows = N / (size - 1) + 1;
-            else 
-                rows = N / (size - 1);
+            lowBound = (i - 1) * numCalculate;
 
-            MPI_Send(&buff, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-            MPI_Send(&rows, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
-            MPI_Send(&mA[buff][0], N * N, MPI_INT, i, 1, MPI_COMM_WORLD);
-            MPI_Send(&mB, N * N, MPI_INT, i, 1, MPI_COMM_WORLD);
-            buff += rows;
-        }
+            if (((i + 1) == size) && ((ROW % (size - 1)) != 0))
+            {// Строки не делятся поровну
+                upBound = ROW; //last slave gets all the remaining rows
+            }
+            else
+            {// Строки делятся поровну
+                upBound = lowBound + numCalculate;
+            }
 
-        for (i = 1; i < size; i++) {
-            MPI_Recv(&buff, 1, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
-            MPI_Recv(&rows, 1, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
-            MPI_Recv(&mResult[buff][0], rows * N, MPI_INT, i, 2, MPI_COMM_WORLD, &status);
+            MPI_Isend(&lowBound, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &request);
+            MPI_Isend(&upBound, 1, MPI_INT, i, 2, MPI_COMM_WORLD, &request);
+            MPI_Isend(&mA[lowBound][0], (upBound - lowBound) * COL, MPI_DOUBLE, i, 3, MPI_COMM_WORLD, &request);
+
         }
-        endTime = MPI_Wtime();
-        printf("\nRunning Time = %f\n\n", endTime - startTime);
     }
-
+    MPI_Bcast(&mB, ROW * COL, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     if (rank > 0)
     {
-        MPI_Recv(&buff, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-        MPI_Recv(&rows, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-        MPI_Recv(&mA[buff][0], N * N, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
-        MPI_Recv(&mB, N * N, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+        
+        MPI_Recv(&lowBound, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
+        MPI_Recv(&upBound, 1, MPI_INT, 0, 2, MPI_COMM_WORLD, &status);
+        MPI_Recv(&mA[lowBound][0], (upBound - lowBound) * COL, MPI_DOUBLE, 0, 3, MPI_COMM_WORLD, &status);
+        printf("rank: %d \tsize: %d - [%d;%d]", rank, size, lowBound, upBound);
+        for (int i = lowBound; i < upBound; i++) {//iterate through a given set of rows of [A]
+            for (int j = 0; j < COL; j++) {//columns of [B]
+                for (int k = 0; k < ROW; k++) {//rows of [B]
+                    mR[i][j] += (mA[i][k] * mB[k][j]);
+                }
+            }
+        }
+        MPI_Isend(&lowBound, 1, MPI_INT, 0, 4, MPI_COMM_WORLD, &request);
+        MPI_Isend(&upBound, 1, MPI_INT, 0, 5, MPI_COMM_WORLD, &request);
+        MPI_Isend(&mR[lowBound][0], (upBound - lowBound) * COL, MPI_DOUBLE, 0, 6, MPI_COMM_WORLD, &request);
 
-        for (i = buff; i < rows; i++)
-            for (k = 0; k < N; k++)
-                for (j = 0; j < N; j++)
-                    mResult[i][j] += (mA[i][k] * mB[k][j]);
-
-        MPI_Send(&buff, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-        MPI_Send(&rows, 1, MPI_INT, 0, 2, MPI_COMM_WORLD);
-        MPI_Send(&(mResult[buff][0]), rows * N, MPI_INT, 0, 2, MPI_COMM_WORLD);
     }
-
-
+    if (rank == 0) 
+    {
+        for (int i = 1; i < size; i++)
+        {
+            MPI_Recv(&lowBound, 1, MPI_INT, i, 4, MPI_COMM_WORLD, &status);
+            MPI_Recv(&upBound, 1, MPI_INT, i, 5, MPI_COMM_WORLD, &status);
+            MPI_Recv(&mR[lowBound][0], (upBound - lowBound) * COL, MPI_DOUBLE, i, 6, MPI_COMM_WORLD, &status);
+        }
+        endTime = MPI_Wtime() - startTime;
+        printf("\nTime Parallel = %f\n\n", endTime);
+        //printArray();
+    }
+    
     MPI_Finalize();
-    return 0;
 
+    
+    return 0;
 }
+
